9a231ed337b4b3e50560b60a22071131b6ea657b
Nilesh Jayanandana <nilesh.jayanandana@yahoo.com>
2017-07-08 20:16:46 +0530

    Entity Retrieval

--- src/main/java/com/akura/retrieval/models/Entity.java
+++ src/main/java/com/akura/retrieval/models/Entity.java
@@ -2,14 +2,22 @@ package com.akura.retrieval.models;
 
 import com.akura.config.Config;
 import org.apache.jena.ontology.Individual;
+import org.apache.jena.ontology.ObjectProperty;
 import org.apache.jena.ontology.OntModel;
+import org.apache.jena.rdf.model.Property;
 
 public class Entity {
 
     private OntModel model;
     public Individual instance;
 
-    Entity(OntModel m) {
+    private Property name;
+    private Property hashID;
+    private ObjectProperty containProperty;
+    private ObjectProperty feature;
+    private ObjectProperty evaluatedBy;
+
+    public Entity(OntModel m) {
         this.model = m;
     }
 
@@ -23,5 +31,21 @@ public class Entity {
     public void searchEntityByString(String name) {
 
         //TODO
+    }
+
+    private void initProperties() {
+        name = model.getProperty(Config.URI_NAMESPACE + "EntityName");
+        hashID = model.getProperty(Config.URI_NAMESPACE + "HashID");
+        containProperty = model.getObjectProperty(Config.URI_NAMESPACE + "ContainProperty");
+        feature = model.getObjectProperty(Config.URI_NAMESPACE + "SubEntity");
+        evaluatedBy = model.getObjectProperty(Config.URI_NAMESPACE + "EvaluatedBy");
+    }
+
+    public String getName() {
+        return this.instance.getProperty(this.name).getLiteral().toString();
+    }
+
+    public String getHash() {
+        return this.instance.getProperty(this.hashID).getLiteral().toString();
     }
 }
--- src/main/java/com/akura/test/RetrievalTest.java
+++ src/main/java/com/akura/test/RetrievalTest.java
@@ -1,8 +1,26 @@
 package com.akura.test;
 
 
+import com.akura.config.Config;
+import com.akura.retrieval.models.Entity;
+import com.akura.utility.HashGeneratorClass;
+import com.akura.utility.OntologyReader;
+import org.apache.jena.ontology.OntModel;
+
 public class RetrievalTest {
     public static void main(String[] args) {
 
+        OntModel m = OntologyReader.getOntologyModel(Config.OWL_FILENAME);
+
+        Entity entity = new Entity(m);
+        entity.getEntityByHash(HashGeneratorClass.generateHashForString("iphone 7", "ENTITY"));
+
+        if (entity.instance != null) {
+
+
+
+            System.out.println(entity.instance);
+
+        }
     }
 }
