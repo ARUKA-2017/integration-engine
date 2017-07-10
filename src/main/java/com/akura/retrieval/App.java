cef2da6d35e71939637b57a5dbac729e30b15291
Nilesh Jayanandana <nilesh.jayanandana@yahoo.com>
2017-07-10 09:35:07 +0530

    enable cors

--- src/main/java/com/akura/retrieval/App.java
+++ src/main/java/com/akura/retrieval/App.java
@@ -3,18 +3,22 @@ package com.akura.retrieval;
 
 import com.akura.mapping.service.MappingService;
 import com.akura.retrieval.service.ProductService;
+import com.akura.retrieval.service.SparkMiddleware;
 import com.google.gson.Gson;
 
- ;
-import static spark.Spark.get;
-import static spark.Spark.post;
+;import static spark.Spark.*;
 
 
 public class App {
-    public static void main(String[] args){
+
+
+    public static void main(String[] args) {
         Gson gson = new Gson();
         ProductService ps = new ProductService();
         MappingService mp = new MappingService();
+
+        SparkMiddleware.enableCORS("*", "POST, GET, OPTIONS, PUT, DELETE",
+                "Content-Type, x-xsrf-token, content-Type, X-Auth-Token, Origin, Authorization");
 
         get("/search", (req, res) -> gson.toJson(ps.searchProduct(req.queryParams("search"), res, false)));
 
