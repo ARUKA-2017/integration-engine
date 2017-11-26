9a2a44c06805400e5e31528df90ef016b1551eaa
Rishanthakumar <rishanthakumar@gmail.com>
2017-11-26 18:04:08 +0530

    Refactor App.

--- src/main/java/com/akura/App.java
+++ src/main/java/com/akura/App.java
@@ -5,6 +5,7 @@ import com.akura.parser.Parser;
 import com.akura.retrieval.service.ProductService;
 import com.akura.retrieval.service.SparkMiddleware;
 import com.google.gson.Gson;
+
 import spark.ModelAndView;
 import spark.Spark;
 import spark.template.jade.JadeTemplateEngine;
@@ -14,8 +15,10 @@ import java.util.HashMap;
 import static spark.Spark.get;
 import static spark.Spark.post;
 
+/**
+ * Main application startup class with spark
+ */
 public class App {
-
 
     public static void main(String[] args) {
         Gson gson = new Gson();
@@ -23,28 +26,22 @@ public class App {
         MappingService mp = new MappingService();
         Spark.staticFileLocation("/public");
 
-
-        get("/json2owl", (req, res) ->new ModelAndView(new HashMap<>(), "j2owl"), new JadeTemplateEngine());
+        get("/json2owl", (req, res) -> new ModelAndView(new HashMap<>(), "j2owl"), new JadeTemplateEngine());
 
         SparkMiddleware.enableCORS("*", "POST, GET, OPTIONS, PUT, DELETE",
                 "Content-Type, x-xsrf-token, content-Type, X-Auth-Token, Origin, Authorization");
 
         get("/search", (req, res) -> gson.toJson(ps.searchProduct(req.queryParams("search"), res, false, mp)));
-        post("/generate-vowl", (req,res)->{
-           String uid =  Parser.parseFromJsonString(req.body());
-            return  uid;
+        post("/generate-vowl", (req, res) -> {
+            String uid = Parser.parseFromJsonString(req.body());
+            return uid;
         });
-
-        //TODO Get suggestions upon keystroke type endpoint
 
         get("/search/:id", (req, res) -> gson.toJson(ps.searchProduct(req.params(":id"), res, true, mp)));
 
         post("/update-ontology", (req, res) -> gson.toJson(mp.map(req.body(), res)));
         post("/update-ontology-adaptor", (req, res) -> gson.toJson(mp.useAdaptor(req.body(), res)));
 
-//        get("/test", (req, res) -> mp.mongoLoader(res));
-
-
-
+//      get("/test", (req, res) -> mp.mongoLoader(res));
     }
 }
